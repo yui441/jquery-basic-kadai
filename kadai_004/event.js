5b479e2162a9926e2eced5aa58e02dcfcef35e98
yui441 <yui.yui.yui.n.18@gmail.com>
2024-10-24 16:30:08 +0900

    kadai_004をアップロード

---
+++ kadai_004/event.js
@@ -0,0 +1,14 @@
+
+
+$(window).on('load', function() {
+    console.log('loadイベントが発生しました');
+  });
+  
+  $(function() {
+    $(window).on('scroll', function() {
+      console.log('scrollイベントが発生しました');
+    });
+  });
+  
+
+
